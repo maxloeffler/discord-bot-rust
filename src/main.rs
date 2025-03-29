d6b6aba90aeb68ad97802dcadce096488af0765d
Maximilian Löffler <s8maloef@stud.uni-saarland.de>
2025-03-29 19:13:14 +0100

    Default 'command_prefix' to '?'

--- src/main.rs
+++ src/main.rs
@@ -54,9 +54,15 @@ async fn main() {
 async fn setup() -> String {
     let token = fs::read_to_string("token.txt").unwrap();
     let config = ConfigDB::get_instance();
+
+    // initialize executed_commands and command_prefix
     if !config.has("executed_commands").await {
         config.set("executed_commands", "0").await;
     }
+    if !config.has("command_prefix").await {
+        config.set("command_prefix", "?").await;
+    }
+
     config.set("uptime", &chrono::Utc::now().timestamp().to_string()).await;
     config.set("token", &token).await;
     config.get("token").await.unwrap().to_string()
